c4541c936284dd3a9a82329cda4ecca96158a756
Genilson fernandes <geniilsonfernandes@gmail.com>
2023-06-23 15:58:50 -0300

    test husk

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
 
 // Boot express
 const app: Application = express();
