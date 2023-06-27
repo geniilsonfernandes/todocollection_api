9438c5e1c671db3581c1db16a5bb0307e834e13d
Genilson fernandes <geniilsonfernandes@gmail.com>
2023-06-27 16:49:16 -0300

    ✨feat:
    add new routes

--- src/routes/collections.routes.ts
+++ src/routes/collections.routes.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express'
 import { CollectionCreateController } from '../controllers/Collection/CollectionCreateController'
 import { CollectionListController } from '../controllers/Collection/CollectionListController'
+import { CollectionUpdateController } from '../controllers/Collection/CollectioUpdateController'
+import { CollectioDeleteController } from '../controllers/Collection/CollectioDeleteController'
 
 const collectionCreateController = new CollectionCreateController()
 const collectionListController = new CollectionListController()
+const collectionUpdateController = new CollectionUpdateController()
+const collectioDeleteController = new CollectioDeleteController()
 
 const collections = Router()
 
-collections.use('/', collectionListController.handle)
-collections.use('/create', collectionCreateController.handle)
+collections.get('/', collectionListController.handle)
+collections.post('/create', collectionCreateController.handle)
+collections.put('/:id', collectionUpdateController.handle)
+collections.delete('/:id', collectioDeleteController.handle)
 
 export { collections }
