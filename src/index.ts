bc5b45c9337855d676c99412c6a86e7a9831805c
Genilson fernandes <geniilsonfernandes@gmail.com>
2023-06-26 21:04:21 -0300

    ♻️refactor:
    config project

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express'
-import { AppDataSource } from './database/data-source'
+import { AppDataSource } from './data-source'
 import { router } from './routes'
 
-const app = express()
-app.use(express.json())
-
-app.use(router)
-
 AppDataSource.initialize().then(() => {
+  const app = express()
+  app.use(express.json())
+  app.use(router)
   app.listen(process.env.PORT)
+
+  app.get('/', (request, response) => {
+    return response.json({ message: 'Hello Worldxcv' })
+  })
 })
+
+
