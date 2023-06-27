d12d3ad951ce20603b78fc4de5d3e9b891ed4fac
Genilson fernandes <geniilsonfernandes@gmail.com>
2023-06-27 16:48:52 -0300

    ✨feat:
    createupdate collections

---
+++ src/controllers/Collection/CollectioUpdateController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express'
+import { collectionsRepository } from '../../repositories/collectionsRepository'
+
+class CollectionUpdateController {
+  async handle(request: Request, response: Response) {
+    const { id } = request.params as { id: string }
+    const { name, description } = request.body
+
+    if (!name || !description) {
+      return response.status(400).json({ message: 'Missing required data' })
+    }
+
+    try {
+      const collection = await collectionsRepository.findOneBy({
+        id,
+      })
+
+      if (!collection) {
+        return response.status(404).json({ message: 'Collection not found' })
+      }
+
+      collection.name = name
+      collection.description = description
+
+      await collectionsRepository.save(collection)
+
+      response.status(200).json({ message: 'Collection updated successfully' })
+    } catch (error) {
+      return response.status(500).json({ message: `Internal Sever Error` })
+    }
+  }
+}
+
+export { CollectionUpdateController }
